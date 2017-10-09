1b00c9cd48ee5ae21cf7fcd61ddc86722aefca88
DanielGreene101 <d.b.greene101@gmail.com>
2017-10-09 12:37:17 -0500

    finished

---
+++ nick.rb
@@ -0,0 +1,15 @@
+songs = [
+    ['Nickelback', 'How You Remind Me'],
+    ['Will.i.am', 'That Power'],
+    ['Miles Davis', 'Stella by Starlight'],
+    ['Nickelback', 'Animals']
+]
+
+finalSongs = []
+
+for song in songs
+	if song[0] == "Nickelback"
+		finalSongs.push(song)
+	end
+end
+	print finalSongs
